0c428009dfef2e956284165caceb2953a4407d0f
drkwdck <reshetnikov.e.a@mail.ru>
2022-03-07 14:53:41 +0300

    quantizer added

--- Quantizer.py
+++ Quantizer.py
@@ -8,3 +8,8 @@ class Quantizer:
     def Quantize(input_vector: np.ndarray) -> np.ndarray:
         quantize_matrix = np.ones(input_vector.shape) * Quantizer.quantize_step
         return np.sign(input_vector) * np.fix(np.abs(input_vector) / quantize_matrix)
+
+    @staticmethod
+    def dequantize(input_vector: np.ndarray) -> np.ndarray:
+        quantize_matrix = np.ones(input_vector.shape) * Quantizer.quantize_step
+        return (input_vector + np.sign(input_vector) * 0.5) * quantize_matrix
